793844a5b94b0b268f48f9f08297818ec2a5c2fd
lilslsls <1057102550@qq.com>
2019-07-20 09:41:40 +0800

    enter

---
+++ numpy3.py
@@ -0,0 +1,9 @@
+from __future__ import print_function
+import numpy as np
+#查看numpy版本
+print(np.__version__)
+
+x1=np.array(['hello','say'],dtype=np.str)
+x2=np.array(['world','something'],dtype=np.str)
+out=np.char.add(x1,x2)
+print(out)
