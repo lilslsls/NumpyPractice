f9972ccb7f2979acde4747cbe3c54f07a5ffdef2
lilslsls <1057102550@qq.com>
2019-07-20 09:51:11 +0800

    enter

--- numpy3.py
+++ numpy3.py
@@ -3,7 +3,7 @@ import numpy as np
 #查看numpy版本
 print(np.__version__)
 
-x1=np.array(['hello','say'],dtype=np.str)
-x2=np.array(['world','something'],dtype=np.str)
-out=np.char.add(x1,x2)
-print(out)
+# x1=np.array(['hello','say'],dtype=np.str)
+# x2=np.array(['world','something'],dtype=np.str)
+# out=np.char.add(x1,x2)
+# print(out)
